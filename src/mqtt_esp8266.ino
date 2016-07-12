6f0e225b30c088bc3ed38d058c023d3ad9cce5dd
Alexandre Leblanc <info@alexandre-leblanc.com>
2016-07-12 10:49:53 -0400

    tokenizer

--- src/mqtt_esp8266.ino
+++ src/mqtt_esp8266.ino
@@ -25,13 +25,17 @@
 #define AIO_SERVERPORT  1883                   // use 8883 for SSL
 #define AIO_USERNAME    ""
 #define AIO_KEY         ""
-typedef struct{
+
+const int CODE_SIZE = 3;
+const String CODE_ARRAY[CODE_SIZE] = {"001", "002", "003"};
+
+struct Token{
   int code;
   int p;
   int i;
   int d;
- }Token;
- 
+ };
+
 /************ Global State (you don't need to change this!) ******************/
 
 WiFiClient client;
@@ -64,14 +68,15 @@ void MQTT_connect();
 
 
 
+String serial_input;
 bool state = false;
 bool lastState = false;
-String serial_input;
 double Reading, Setpoint = 96, Input, Output;
 double Kp=7, Ki=97, Kd=18;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 int WindowSize = 5000;
 unsigned long windowStartTime;
+
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
 
@@ -104,7 +109,7 @@ void loop() {
   if (state) {
 
     digitalWrite(SSR_ALIM, HIGH);
-    
+
     if (!lastState){
          Serial.println("PID ON");
         windowStartTime = millis();
@@ -113,7 +118,7 @@ void loop() {
         myPID.SetOutputLimits(0, WindowSize);
 
         //turn the PID on
-        myPID.SetMode(AUTOMATIC);     
+        myPID.SetMode(AUTOMATIC);
     }
 
     Input = thermocouple.readCelsius();
@@ -127,17 +132,17 @@ void loop() {
     if(Output < millis() - windowStartTime){
         digitalWrite(SSR_PID, HIGH);
         Serial.println("PID HIGH");
-    }else{ 
+    }else{
         digitalWrite(SSR_PID, LOW);
         Serial.println("PID LOW");
     }
-        
+
     temps.publish(Input);
     espressoOut.publish("ON");
 
     Serial.print("TEMPS");
     Serial.println(Input);
-    
+
     lastState =  state;
 
   } else {
@@ -148,33 +153,105 @@ void loop() {
   }
   MQTT_manager();
 
-  getString(serial_input);
-
-  manageCommand(serial_input);
+  //serial_input = getString(serial_input);
+  manageCommand("001 10 10 10");
 
 }
 
-Token parseCommand(String & s){
-    for (int i =0 ; i< CODE_SIZE; i++){
-       if(s.startsWith(CODE_ARRAY[i])){
-                
-       }
+void setToken(int * ptr_i, String s){
+    long int i = s.toInt();
+    if (i!=0){
+        *ptr_i = (int)i;
     }
 }
-void manageCommand(String & s){
-   
+
+Token parseCommand2(String s){
+    unsigned int state = 0;
+    unsigned int count = 0;
+    unsigned int wordCount = 0;
+    String buffer = "";
+    Token t;
+
+    for(unsigned int i = 3; i < s.length(); i++){
+        char c = s.charAt(i);
+        switch (state) {
+            case 0:
+                if(c != ' '){
+                    t.code = -10;
+                    return t;
+                }
+                state = 1;
+                break;
+            case 1:
+                if (!(c >= 48  && c <=57)){
+                    t.code = -11;
+                    return t;
+                }
+                buffer += c;
+                if(s.charAt(i+1) == ' ' || i == s.length()-1 ){
+                    switch (++wordCount) {
+                        case 1:
+                            setToken(&t.p, buffer);
+                            break;
+                        case 2:
+                            setToken(&t.i, buffer);
+                            break;
+                        case 3:
+                            setToken(&t.d, buffer);
+                            break;
+                        default:
+                            t.code = -12;
+                            return t;
+                    }
+                    count = 0;
+                    state = 0;
+                    buffer = "";
+                }
+                break;
+        }
+    }
+    return t;
+
+
 }
 
-void getString(String & s){
-    
+Token parseCommand(String s){
+    for (unsigned int i = 0 ; i< CODE_SIZE; i++){
+       if(s.startsWith(CODE_ARRAY[i])){
+             Token t = parseCommand2(s);
+
+             if (!(t.code < 0)){
+                long int code = s.substring(0,3).toInt();
+                if(code != 0){
+                    t.code = code;
+                }else{
+                    Serial.println("error toInt()");
+                }
+                return t;
+             }else{
+                Serial.println("error parse command");
+                return t;
+             }
+        }
+    }
+}
+
+void manageCommand(String s){
+    Token t = parseCommand(s);
+    Serial.printf("code pid %d %d %d %d ", t.code, t.p, t.i, t.d );
+}
+
+String getString(String s){
+
     if (Serial.available()){
         char c;
         c = Serial.read();
         while(c != '\n' || c != '\r'){
             s += c;
             c = Serial.read();
-        }   
+        }
     }
+    return s;
 }
 
 void MQTT_manager(){
@@ -196,7 +273,7 @@ void MQTT_manager(){
   if (! mqtt.ping()) {
     mqtt.disconnect();
   }
-    
+
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
@@ -225,5 +302,3 @@ void MQTT_connect() {
   }
   Serial.println("MQTT Connected!");
 }
-
-
